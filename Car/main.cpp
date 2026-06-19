5187b36317be22831c102e6079de460f42085826
Hrapraiter <223995225+Hrapraiter@users.noreply.github.com>
2026-06-19 17:56:33 +0700

    OutConsole lag fix Speed Control And Simple Fisical DONE

--- Car/main.cpp
+++ Car/main.cpp
@@ -1,7 +1,9 @@
 #include <Windows.h>
 #include <iostream>
+#include <string>
 #include <conio.h>
 #include <thread>
+#include <mutex>
 #include <chrono>
 using std::cin;
 using std::cout;
@@ -87,6 +89,18 @@ public:
 	{
 		is_started = false;
 	}
+	void consumption_to_speed(const int& speed)
+	{
+		static bool(*range)(int, int , int) = [](int start, int end , int value)
+			{
+				return value >= start && value <= end;
+			};
+		if (range(1, 60, speed))			consumption_per_second = 0.0020;
+		else if (range(61, 100, speed))		consumption_per_second = 0.0014;
+		else if (range(101 , 140, speed))	consumption_per_second = 0.0020;
+		else if (range(141, 200, speed))	consumption_per_second = 0.0025;
+		else if (range(200, 250, speed))	consumption_per_second = 0.0030;
+	}
 	bool started() const { return is_started; }
 	double get_consumption_per_second() const { return consumption_per_second; }
 	void info()const
@@ -99,6 +113,9 @@ class Car
 {
 	Engine engine;
 	Tank tank;
+	int speed;
+	const int speed_tick = 2;
+	const int max_speed = 250;
 	bool driver_inside;
 	struct
 	{
@@ -109,6 +126,7 @@ public:
 	Car(double consumption, int capacity = 50) :engine(consumption), tank(capacity)
 	{
 		driver_inside = false;
+		speed = 0;
 		cout << "Your car is ready to go, press Enter to get in \t" << this << endl;
 	}
 	~Car()
@@ -158,15 +176,61 @@ public:
 				if (driver_inside)get_out();
 				else get_in();
 				break;
+			case 'W':
+			case 'w':
+				if (driver_inside && engine.started())
+				{
+					speed += speed_tick;
+					if (speed > 250)speed = 250;
+					engine.consumption_to_speed(speed);
+				}
+				break;
+			case 'S':
+			case 's':
+				if (driver_inside && engine.started())
+				{
+					speed -= speed_tick * 2;
+					if (speed < 0)speed = 0;
+					engine.consumption_to_speed(speed);
+				}
+				break;
 			case 'F':
 			case 'f':
+			{
+				CONSOLE_SCREEN_BUFFER_INFO info;
 				if (!driver_inside || !engine.started())
 				{
 					double amount;
+					if (driver_inside)
+					{
+						mut_cout.lock();
+
+						GetConsoleScreenBufferInfo(hConsole, &info);
+						SetConsoleCursorPosition(hConsole, cords.end);
+						cout << "¬ведите объЄм топлива: "; cin >> amount;
+						tank.fill(amount);
+						SetConsoleCursorPosition(hConsole, info.dwCursorPosition);
+
+						mut_cout.unlock();
+						realocate_out_driver_inside();
+						break;
+					}
 					cout << "¬ведите объЄм топлива: "; cin >> amount;
 					tank.fill(amount);
 				}
-				else cout << "нужно заглушить двигатель и выйти из машины, у нас только самообслуживание." << endl;
+				else
+				{
+					mut_cout.lock();
+
+					GetConsoleScreenBufferInfo(hConsole, &info);
+					SetConsoleCursorPosition(hConsole, cords.end);
+					cout << "нужно заглушить двигатель и выйти из машины, у нас только самообслуживание." << endl;
+					SetConsoleCursorPosition(hConsole, info.dwCursorPosition);
+
+					mut_cout.unlock();
+					realocate_out_driver_inside();
+				}
+			}
 				break;
 			case 'I':
 			case 'i':
@@ -177,6 +241,14 @@ public:
 				shutdown();
 				get_out();
 				break;
+			}
+			static std::chrono::time_point<std::chrono::system_clock> last = std::chrono::system_clock::now();
+			if (speed && std::chrono::system_clock::now() - last >= 1s)
+			{
+				speed -= 1;
+				if (!speed)speed = 0;
+			    engine.consumption_to_speed(speed);
+				last = std::chrono::system_clock::now();
 			}
 			if (tank.get_fuel_level() == 0 && engine.started())shutdown();
 		} while (key != Escape);
@@ -189,7 +261,63 @@ public:
 	void panel()
 	{
 		HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-		while (driver_inside)
+		realocate_out_driver_inside();
+		while(driver_inside)
+		{
+			if(cashe.tank_fuel_level != tank.get_fuel_level())
+			{
+				mut_cout.lock();
+				SetConsoleCursorPosition(hConsole, cords.fuel_level);
+				cout << (cashe.tank_fuel_level = tank.get_fuel_level());
+				mut_cout.unlock();
+				
+			}
+			if(cashe.low_fuel != tank.get_fuel_level() < 5)
+			{
+				mut_cout.lock();
+				SetConsoleCursorPosition(hConsole, cords.low_fuel);
+				if ((cashe.low_fuel = tank.get_fuel_level() < 5))
+				{
+					SetConsoleTextAttribute(hConsole, 0x4F);
+					cout << " LOW FUEL ";
+					SetConsoleTextAttribute(hConsole, 0x07);
+				}
+				mut_cout.unlock();
+			}
+			if(cashe.car_speed != speed)
+			{
+				mut_cout.lock();
+				SetConsoleCursorPosition(hConsole, cords.car_speed);
+				cout << (cashe.car_speed = speed) << "   ";
+				mut_cout.unlock();
+			}
+			if(cashe.engine_is_started != engine.started())
+			{
+				mut_cout.lock();
+				SetConsoleCursorPosition(hConsole, cords.engine_is_started);
+				if ((cashe.engine_is_started = engine.started()))
+				{
+					SetConsoleTextAttribute(hConsole, 0x0A);
+					cout << " STARTED ";
+					SetConsoleTextAttribute(hConsole, 0x07);
+				}
+				else
+				{
+					SetConsoleTextAttribute(hConsole, 0x04);
+					cout << " STOPED ";
+					SetConsoleTextAttribute(hConsole, 0x07);
+				}
+				mut_cout.unlock();
+			}
+			if(cashe.engine_consumption != engine.get_consumption_per_second())
+			{
+				mut_cout.lock();
+				SetConsoleCursorPosition(hConsole, cords.engine_consumption);
+				cout << (cashe.engine_consumption = engine.get_consumption_per_second()) << "  ";
+				mut_cout.unlock();
+			}
+		}
+		/*while (driver_inside)
 		{
 			system("CLS");
 			cout << "Fuel level: " << tank.get_fuel_level() << " liters.\t";
@@ -200,11 +328,12 @@ public:
 				SetConsoleTextAttribute(hConsole, 0x07);
 			}
 			cout << endl;
+			cout << "Speed: \t" << speed << " km/h.\n";
 			cout << "Engine is ";
 			if(engine.started())
 			{
 				SetConsoleTextAttribute(hConsole, 0x0A);
-				cout << " STARTED " << endl;
+				cout << " STARTED ";
 				SetConsoleTextAttribute(hConsole, 0x07);
 			}
 			else
@@ -213,9 +342,84 @@ public:
 				cout << " STOPED ";
 				SetConsoleTextAttribute(hConsole, 0x07);
 			}
+			cout << endl;
+			cout << "Engine consumption: \t" << engine.get_consumption_per_second() << endl;
 			std::this_thread::sleep_for(100ms);
-		}
+		}*/
 	}
+private:
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	struct
+	{
+		COORD fuel_level;
+		COORD low_fuel;
+		COORD car_speed;
+		COORD engine_is_started;
+		COORD engine_consumption;
+		COORD end;
+	}cords;
+	struct
+	{
+		double tank_fuel_level;
+		bool low_fuel;
+		int car_speed;
+		bool engine_is_started;
+		double engine_consumption;
+	}cashe{0,0,0,0,0};
+	std::mutex mut_cout;
+	void realocate_out_driver_inside()
+	{
+		if (!driver_inside)return;
+		mut_cout.lock();
+
+		system("CLS");
+		cout << "Fuel level: ";
+		CONSOLE_SCREEN_BUFFER_INFO info;
+		GetConsoleScreenBufferInfo(hConsole, &info);
+		cords.fuel_level = info.dwCursorPosition;
+		cout << cashe.tank_fuel_level << "\t\tliters.\t";
+
+		GetConsoleScreenBufferInfo(hConsole, &info);
+		cords.low_fuel = info.dwCursorPosition;
+		if (cashe.low_fuel)
+		{
+			SetConsoleTextAttribute(hConsole, 0x4F);
+			cout << " LOW FUEL ";
+			SetConsoleTextAttribute(hConsole, 0x07);
+		}
+		cout << endl;
+
+		cout << "Speed: \t    ";
+		GetConsoleScreenBufferInfo(hConsole, &info);
+		cords.car_speed = info.dwCursorPosition;
+		cout << cashe.car_speed << "\t\tkm/h.\n";
+
+		cout << "Engine is ";
+		GetConsoleScreenBufferInfo(hConsole, &info);
+		cords.engine_is_started = info.dwCursorPosition;
+		if (cashe.engine_is_started)
+		{
+			SetConsoleTextAttribute(hConsole, 0x0A);
+			cout << " STARTED ";
+			SetConsoleTextAttribute(hConsole, 0x07);
+		}
+		else
+		{
+			SetConsoleTextAttribute(hConsole, 0x04);
+			cout << " STOPED ";
+			SetConsoleTextAttribute(hConsole, 0x07);
+		}
+		cout << endl;
+
+		cout << "Engine consumption: \t";
+		GetConsoleScreenBufferInfo(hConsole, &info);
+		cords.engine_consumption = info.dwCursorPosition;
+		cout << cashe.engine_consumption << "\tliters/sec." << endl;
+		GetConsoleScreenBufferInfo(hConsole, &info);
+		cords.end = info.dwCursorPosition;
+
+		mut_cout.unlock();
+	};
 };
 
 //#define TANK_CHECK
